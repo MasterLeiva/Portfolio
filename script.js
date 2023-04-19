c10585c1a14acfbe656fff2a2b185ff289b4ee86
Leiva <davidleiva11@icloud.com>
2023-04-18 18:05:27 -0600

    update working menu

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const menuToggle = document.querySelector('.hamburguer');
-const modal = document.querySelector('.modal');
-const closeButton = document.querySelector('.close-button');
+var openBtn = document.querySelector('#openBtn');
+var closeBtn = document.querySelector('#close');
+var linkNavPort = document.querySelector('#linkNav-port');
+var linkNavAbout = document.querySelector('#linkNav-about');
+var linkNavCont = document.querySelector('#linkNav-cont');
+var windowUl = document.querySelector('.links');
 
-menuToggle.addEventListener('click', () => {
-  modal.classList.add('show');
-});
+function openWindow() {
+  windowUl.classList.add('show');
+}
 
-closeButton.addEventListener('click', () => {
-  modal.classList.remove('show');
-});
+function closeWindow() {
+  windowUl.classList.remove('show');
+}
 
-modal.querySelectorAll('a').forEach((link) => {
-  link.addEventListener('click', () => {
-    modal.classList.remove('show');
-  });
-});
+closeBtn.addEventListener('click', closeWindow);
+openBtn.addEventListener('click', openWindow);
+linkNavPort.addEventListener('click', closeWindow);
+linkNavAbout.addEventListener('click', closeWindow);
+linkNavCont.addEventListener('click', closeWindow);
